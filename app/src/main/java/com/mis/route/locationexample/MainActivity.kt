514b3bb0ea9eb33849c07c45ad52bb64bf199150
Mahmoud-Ibrahim-750 <104442642+Mahmoud-Ibrahim-750@users.noreply.github.com>
2024-02-19 15:10:06 +0200

    Location Updates Setup

--- app/src/main/java/com/mis/route/locationexample/MainActivity.kt
+++ app/src/main/java/com/mis/route/locationexample/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.mis.route.locationexample
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContracts
-import com.mis.route.locationexample.databinding.ActivityMainBinding
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsResponse
+import com.google.android.gms.location.Priority
+import com.google.android.gms.location.SettingsClient
+import com.google.android.gms.tasks.Task
+import com.mis.route.locationexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -17,14 +34,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var locationPermissionRequest: ActivityResultLauncher<Array<String>>
     private lateinit var locationPermissionRationaleDialog: AlertDialog
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
 
 
     private fun askForUpdate() {
         Toast.makeText(this, "update first", Toast.LENGTH_SHORT).show()
     }
 
-    private fun startAction() {
-        Toast.makeText(this, "start", Toast.LENGTH_SHORT).show()
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
     }
 
     private fun locationPermissionsGranted(): Boolean {
@@ -38,6 +63,7 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
@@ -58,13 +84,15 @@ class MainActivity : AppCompatActivity() {
             "No thanks"
         )
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         locationPermissionRequest = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) { permissions ->
             when {
                 permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
                     // Precise location access granted.
-                    startAction()
+                    startLocationUpdates()
                 }
 
                 permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
@@ -86,10 +114,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    // Update UI with location data
+                    Log.e("loc", "${location.latitude} ${location.longitude}")
+                }
+            }
+        }
+
+        createLocationRequest()
+
 
         binding.startListeningBtn.setOnClickListener {
             if (locationPermissionsGranted()) {
-                startAction()
+                startLocationUpdates()
             } else {
                 locationPermissionRequest.launch(
                     arrayOf(
@@ -102,9 +141,64 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
+    fun createLocationRequest() {
+        locationRequest = LocationRequest.Builder(1000)
+            .setMinUpdateIntervalMillis(5000)
+            .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+            .build()
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+
+        task.addOnCompleteListener { locationSettingsResponse ->
+            if (locationSettingsResponse.isSuccessful) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+            } else {
+                val exception = locationSettingsResponse.exception
+                if (exception is ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        exception.startResolutionForResult(
+                            this@MainActivity,
+                            REQUEST_CHECK_SETTINGS
+                        )
+                    } catch (sendEx: IntentSender.SendIntentException) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        const val REQUEST_CHECK_SETTINGS = 5
     }
 }
